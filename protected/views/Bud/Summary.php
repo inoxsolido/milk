fa2f5d437a9b53b3636470ddc1a9fbe6d25eca90
702 <702@702-13>
2016-01-28 14:32:35 +0700

    ฟอร์มแยก ฝ่ายแผนก

---
+++ protected/views/Bud/Summary.php
@@ -0,0 +1,29 @@
+<div class="container">
+    <div class="form">
+        <form id="frmEx" action="#" method="post" class="form form-horizontal">
+            <div class="form-group">
+                <select id="exwh" class="form-control">
+                    <option value="over">ภาพรวม</option>
+                    <option value="month_goal">เป้ารายเดือน</option>
+                    <option value="erp">ERP</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label class="control-label"><input id="extype" name="extype" type="radio" class=""> รายฝ่าย</label>
+                <label class="control-label"><input id="extype" name="extype" type="radio" class=""> รายแผนก/กอง</label>
+            </div>
+            <div class="form-group" id="exgyear">
+                <select id="exyear" class="form-control">
+                <?php foreach($years as $year){ ?>
+                    <option value="<?=$year['year']?>"><?=($year['year']+543)?></option>
+                <?php } ?>
+                </select>
+            </div>
+            <div class="form-group" id="exgtar" style="display:none">
+                <select id="extar" class="form-control"></select>
+            </div>
+        </form>
+    </div>
+</div>
+
+<script src="<?=Yii::app()->request->baseUrl?>/assets/js/summary.js">
